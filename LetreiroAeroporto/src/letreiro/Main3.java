3f8978baf83c13fc2860b1d10fd1288b15cf86e6
Bartô Alheiros <alheirosb@gmail.com>
2019-11-23 22:05:34 -0300

    syncronized("Thread Safe")

--- LetreiroAeroporto/src/letreiro/Main3.java
+++ LetreiroAeroporto/src/letreiro/Main3.java
@@ -4,7 +4,6 @@ import java.util.concurrent.*;
 
 public class Main3 {
 	
-	static final int BUFF_LEN = 4;
 	public static Semaphore sem = new Semaphore(1);
 	public static Semaphore sem2 = new Semaphore(0);
 
@@ -16,90 +15,91 @@ public class Main3 {
 		}
 	}
 
-	private static void addProc(HighLevelDisplay d) {
+	private static synchronized void addProc(HighLevelDisplay d) {
 
-		try {
+	/*	try {
 			sem.acquire();
 
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	*/	
+		
 		d.addRow("AAAAAAAAAAAAAAAAA");
 		nap(1500);
+		
+	/*	try {
+			sem2.acquire();
+		} catch (InterruptedException e3) {
+			// TODO Auto-generated catch block
+			e3.printStackTrace();
+		}
 		sem.release();
-		sem2.release();
+		
+		try {
+			sem.acquire();
+		} catch (InterruptedException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}*/
+		
 		d.addRow("CCCCCCCCCCCCCCCCC");
 		nap(1500);
+		/*sem.release();
+		sem2.release();
+		
 		try {
 			sem.acquire();
 		} catch (InterruptedException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
-		}
-		
-		sem2.release();
+		}*/
 		
 		d.addRow("JJJJJJJJJJJJJJJJJ");
 		nap(1500);
+		/*sem.release();
+		
 		try {
 			sem.acquire();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
 		d.addRow("YYYYYYYYYYYYYYYYY");
 		nap(1500);
-		
-		
+		//sem2.release();
+	
 	
 		
 	}
 
-	private static void deleteProc(HighLevelDisplay d) {
+	private static synchronized void deleteProc(HighLevelDisplay d) {
 
-		try {
-
+		d.deleteRow(0);
+		nap(5000);
+		//sem2.release();
+		
+		
+		d.deleteRow(0);
+		nap(5000);
+		//sem2.release();
+		
+		/*try {
 			sem2.acquire();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		}*/
+		d.deleteRow(0);
+		nap(10000);
+		//sem2.release();
+		
 		
 		d.deleteRow(0);
 		nap(1500);
+		//sem2.release();
 		
-		try {
-			sem.acquire(1);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		d.deleteRow(0);
-		
-		try {
-			sem.acquire(1);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		d.deleteRow(0);
-		nap(1500);
-		
-		try {
-			sem.acquire(1);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		d.deleteRow(0);
-		nap(1500);
-		
-		sem.release(4);
-
-
-
 	}
 
 	public static void main(String [] args) {
